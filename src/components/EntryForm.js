389b7c97f2823c091f06b4e87ef0ea466fdea29f
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-02-16 10:51:02 -0800

    #1 added all pertinent form fields

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import MomentUtils from '@date-io/moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { FormControl, FormControlLabel, Grid, InputLabel, MenuItem, Select, Switch } from '@material-ui/core';
+import { Button, FormControl, FormControlLabel, Grid, InputLabel, MenuItem, Select, Switch, TextField } from '@material-ui/core';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 
 
@@ -11,18 +11,24 @@ const useStyles = makeStyles((theme) => ({
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center',
         flexWrap: 'wrap',
         '& > *': {
             margin: theme.spacing(1),
-            width: '55%',
+            width: '50%',
             height: '80%',
         },
     },
     formControl: {
-        marginTop: '15px',
         minWidth: 150,
     },
+    grid: {
+        display: 'flex',
+        alignItems: 'center',
+        marginTop: '20px',
+    },
+    commentTextBox: {
+        width: '90%',
+    }
 }));
 
 const EntryForm = () => {
@@ -31,6 +37,7 @@ const EntryForm = () => {
     const [feedbackType, setFeedbackType] = useState('');
     const [anonymous, setAnonymous] = useState(false);
     const [rank, setRank] = useState('');
+    const [wantsContact, setWantsContact] = useState(false);
 
 
     // haha event handlers go brrr
@@ -38,8 +45,8 @@ const EntryForm = () => {
         setSelectedDate(date);
     };
 
-    const handleFeedbackTypeChange = (type) => {
-        setFeedbackType(type.target.value);
+    const handleFeedbackTypeChange = (event) => {
+        setFeedbackType(event.target.value);
     };
 
     const handleAnonSwitch = (event) => {
@@ -50,82 +57,156 @@ const EntryForm = () => {
         setRank(event.target.value);
     };
 
+    const handleContactSwitch = (event) => {
+        setWantsContact(event.target.checked);
+    }
+
 
     return (
         <form className={classes.root}>
             <Paper elevation={3}>
                 <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <Grid container spacing={1}>
+
+                        <Grid
+                            container
+                            justify='space-around'
+                            className={classes.grid}
+                        >
+                            <Grid item>
+                                <KeyboardDatePicker
+                                    className={classes.formControl}
+                                    disableToolbar
+                                    variant='inline'
+                                    format='MM/DD/yyyy'
+                                    margin='normal'
+                                    label='feedback-date'
+                                    value={selectedDate}
+                                    onChange={handleDateChange}
+                                    KeyboardButtonProps={{
+                                        'aria-label': 'change date',
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <FormControl variant='outlined' className={classes.formControl}>
+                                    <InputLabel id='type-select-label'>Feedback Type</InputLabel>
+                                    <Select
+                                        labelId='type-select-label'
+                                        value={feedbackType}
+                                        onChange={handleFeedbackTypeChange}
+                                        label='Feedback Type'
+                                    >
+                                        <MenuItem value=''>
+                                            ----
+                                    </MenuItem>
+                                        <MenuItem value='praise'>Praise</MenuItem>
+                                        <MenuItem value='complaint'>Complaint</MenuItem>
+                                        <MenuItem value='peer review'>Peer Review</MenuItem>
+                                        <MenuItem value='comment'>Comment</MenuItem>
+                                        <MenuItem value='request'>Request</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <FormControlLabel
+                                    className={classes.formControl}
+                                    control={
+                                        <Switch
+                                            checked={anonymous}
+                                            onChange={handleAnonSwitch}
+                                            name='anonymousCheckbox'
+                                            color='primary'
+                                        />
+                                    }
+                                    label='Submit Anonymously?'
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid
+                            container
+                            justify='space-around'
+                            className={classes.grid}
+                        >
+                            <Grid item>
+                                <FormControl variant='outlined' className={classes.formControl}>
+                                    <InputLabel id='rank-select-label'>Rank</InputLabel>
+                                    <Select
+                                        labelId='rank-select-label'
+                                        value={rank}
+                                        onChange={handleRankSelect}
+                                        label='Rank'
+                                    >
+                                        <MenuItem value=''>
+                                            ----
+                                    </MenuItem>
+                                        <MenuItem value='e1'>E-1</MenuItem>
+                                        <MenuItem value='e2'>E-2</MenuItem>
+                                        <MenuItem value='e3'>E-3</MenuItem>
+                                        <MenuItem value='e4'>E-4</MenuItem>
+                                        <MenuItem value='e5'>E-5</MenuItem>
+
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            <Grid item>
+                                <TextField label='First Name' variant='outlined' />
+                            </Grid>
+                            <Grid item>
+                                <TextField label='Last Name' variant='outlined' />
+                            </Grid>
+                        </Grid>
+                        <Grid
+                            container
+                            justify='space-around'
+                            className={classes.grid}
+                        >
+                            <Grid item>
+                                <TextField label='Email' variant='outlined' />
+                            </Grid>
+                            <Grid item>
+                                <TextField label='Phone Number' variant='outlined' />
+                            </Grid>
+                        </Grid>
+                    </Grid>
                     <Grid
                         container
                         justify='space-around'
+                        className={classes.grid}
                     >
-                        <KeyboardDatePicker
-                            className={classes.formControl}
-                            disableToolbar
-                            variant='inline'
-                            format='MM/DD/yyyy'
-                            margin='normal'
-                            label='feedback-date'
-                            value={selectedDate}
-                            onChange={handleDateChange}
-                            KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                            }}
-                        />
-                        <FormControl variant='outlined' className={classes.formControl}>
-                            <InputLabel id='type-select-label'>Feedback Type</InputLabel>
-                            <Select
-                                labelId='type-select-label'
-                                value={feedbackType}
-                                onChange={handleFeedbackTypeChange}
-                                label='Feedback Type'
-                            >
-                                <MenuItem value=''>
-                                    ----
-                                    </MenuItem>
-                                <MenuItem value='praise'>Praise</MenuItem>
-                                <MenuItem value='complaint'>Complaint</MenuItem>
-                                <MenuItem value='peer review'>Peer Review</MenuItem>
-                                <MenuItem value='comment'>Comment</MenuItem>
-                                <MenuItem value='request'>Request</MenuItem>
-                            </Select>
-                        </FormControl>
                         <FormControlLabel
                             className={classes.formControl}
                             control={
                                 <Switch
-                                    checked={anonymous}
-                                    onChange={handleAnonSwitch}
-                                    name='anonymousCheckbox'
+                                    checked={wantsContact}
+                                    onChange={handleContactSwitch}
+                                    name='contactMeSwitch'
                                     color='primary'
                                 />
                             }
-                            label='Submit Anonymously?'
+                            label='I would like to be contacted about my feedback.'
                         />
                     </Grid>
                     <Grid
                         container
                         justify='space-around'
+                        className={classes.grid}
                     >
-                        <FormControl variant='outlined' className={classes.formControl}>
-                            <InputLabel id='rank-select-label'>Rank</InputLabel>
-                            <Select
-                                labelId='rank-select-label'
-                                value={rank}
-                                onChange={handleRankSelect}
-                                label='Rank'
-                            >
-                                <MenuItem value=''>
-                                    ----
-                                    </MenuItem>
-                                <MenuItem value='e1'>E-1</MenuItem>
-                                <MenuItem value='e2'>E-2</MenuItem>
-                                <MenuItem value='e3'>E-3</MenuItem>
-                                <MenuItem value='e4'>E-4</MenuItem>
-                                <MenuItem value='e5'>E-5</MenuItem>
-
-                            </Select>
-                        </FormControl>
+                        <TextField
+                            className={classes.commentTextBox}
+                            label='Comments'
+                            multiline
+                            rows={8}
+                            placeholder='Enter your feedback here...'
+                            variant='outlined'
+                        />
+                    </Grid>
+                    <Grid
+                        container
+                        justify='space-around'
+                        className={classes.grid}
+                    >
+                        <Button variant='contained' color='primary' size='large'>Submit</Button>
                     </Grid>
                 </MuiPickersUtilsProvider>
             </Paper>
